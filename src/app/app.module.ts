f934ff0a90caa42d45e85517d9ae8ef4235e8327
AndersonAlvarenga <andersonalvarenga15@gmail.com>
2020-04-25 13:51:25 -0300

    Segunda parte da atividade, criação do componente endereço ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { DataComponent } from './data/data.component';
+import { EnderecoComponent } from './endereco/endereco.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventosComponent,
+    DataComponent,
+    EnderecoComponent
   ],
   imports: [
     BrowserModule,
